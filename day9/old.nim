543af58b52c2ac1e10a154fe03343f6bad0c7ef5
Samuel Taylor <0xsmt97@gmail.com>
2020-12-08 23:26:05 -0600

    ugly day9 solution, exploits blocks in nim

---
+++ day9/old.nim
@@ -0,0 +1,30 @@
+import sequtils, strutils, os, math
+
+let preamble = 25
+
+let numbers = paramStr(1).open.readAll.splitLines.map(parseInt)
+var usables = numbers[0..<preamble]
+
+var secret = 0
+
+block outer:
+    for idx, num in numbers[preamble..^1].pairs:
+        block inner:
+            for i1, n1 in usables.pairs:
+                for n2 in usables[i1..^1]:
+                    if n1 + n2 == num:
+                        usables = numbers[idx + 1..^1]
+                        break inner
+            
+            echo num
+            secret = num
+            break outer
+
+block outer:
+    for start, num1 in numbers.pairs:
+        for fin, num2 in numbers[(start)..^1].pairs:
+            let true_fin = start + fin
+            let s = numbers[start..(true_fin)]
+            if s.sum == secret:
+                echo s.max + s.min
+                break outer
